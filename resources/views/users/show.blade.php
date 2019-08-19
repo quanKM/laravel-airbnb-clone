cb00563f9cb4652b4fd6b6427eebf3c79b80db97
Minh Quan <quankhongminh109@gmail.com>
2019-08-19 11:25:55 +0800

    Add room listings in user profile page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,16 +13,40 @@
             <div class="card profile mt-4">
                 <div class="card-header">Verification</div>
                 <div class="card-body">
-                    Email address <br>
-                    Phone num
+                    <ul class="sidebar list-unstyled">
+                        @if ($user->socialAccount)
+                            <li class="p-0">Facebook<span class="float-right"><i class="far fa-check-circle text-secondary"></i></span></li>
+                        @else
+                            <li class="p-0">Email Address<span class="float-right"><i class="far fa-check-circle text-secondary"></i></span></li>
+                        @endif
+                        <li class="p-0">Phone Number<span class="float-right"><i class="far fa-check-circle text-secondary"></i></span></li>
+                    </ul>
                 </div>
             </div>
         </div>
         <div class="col-md-9">
             <h2>{{ $user->name }}</h2>
 
-            <div class="description">
+            <div class="description my-4">
                 {{ $user->description }}
+            </div>
+
+            <h4>Listings ({{ $user->rooms->count() }})</h4>
+            <br/>
+
+            <div class="row">
+                @foreach ($user->rooms as $room)
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-header p-0">
+                                <img class="w-100" src="{{ $room->coverPhoto('medium') }}" alt="">
+                            </div>
+                            <div class="card-body">
+                                <a href="{{ route('rooms.show', $room) }}">{{ $room->listing_name }}</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
